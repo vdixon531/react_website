a62893f2da8a0b64000ea56aacd8d49050519be0
Valmik Dixon <vdixon531@gmail.com>
2023-09-15 11:06:59 -0400

    changed web adress

--- src/components/AdvGallery.js
+++ src/components/AdvGallery.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './AdvGallery.css'
 import Carousel from 'better-react-carousel'
 import s1Image from "../assets/images/img-S1-office.jpg"
 import coImage from "../assets/images/img-slf-cops.jpg"
@@ -6,21 +7,23 @@ import doImage from "../assets/images/img-slf-office.jpg"
 
 const AdvGallery = () => {
   return (
-    <Carousel cols={2} rows={1} gap={10} loop>
-      <Carousel.Item>
-        <img width="100%" src={s1Image} />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img width="100%" src={coImage} />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img width="100%" src={doImage} />
-      </Carousel.Item>
-      <Carousel.Item>
-        {/* anything you want to show in the grid */}
-      </Carousel.Item>
-      {/* ... */}
-    </Carousel>
+    <div className="adv_carousel">
+      <Carousel cols={2} rows={1} gap={10} loop>
+        <Carousel.Item>
+          <img width="100%" src={s1Image} />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img width="100%" src={coImage} />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img width="100%" src={doImage} />
+        </Carousel.Item>
+        <Carousel.Item>
+          {/* anything you want to show in the grid */}
+        </Carousel.Item>
+        {/* ... */}
+      </Carousel>
+      </div>
   )
 }
 
