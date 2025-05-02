7561fc4cbf583a3d6fdae85eb28991a563fa44fd
Valmik Dixon <vdixon531@gmail.com>
2025-05-02 11:41:41 -0400

    add posthog?

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { PostHogProvider } from 'posthog-js/react';
+
+const options = {
+    api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <React.StrictMode>
+        <PostHogProvider
+            apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+            options={options}
+        >
+            <App />
+        </PostHogProvider>
+    </React.StrictMode>
 );
 
 reportWebVitals();
